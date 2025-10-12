efef695ff7795617b5b4ef158e9b5e06f53bd05c
Aunders Hallsten <aehallsten@gmail.com>
2025-10-12 14:25:49 -0600

    Working on librarizing BTS7960

---
+++ lib/BTS7960/BTS7960.cpp
@@ -0,0 +1,74 @@
+/*
+  BTS7960.cpp - Library to control the BTS7960 motor driver
+  Created by Christoph Eickhoff, August 28, 2018.
+  Released into the public domain.
+*/
+#include "BTS7960.h"
+
+BTS7960::BTS7960(uint8_t RPWM, uint8_t LPWM, uint8_t L_EN, uint8_t R_EN, uint8_t L_IS, uint8_t R_IS)
+{
+    _RPWM = RPWM;
+    _LPWM = LPWM;
+    _L_EN = L_EN;
+    _R_EN = R_EN;
+    _L_IS = L_IS;
+    _R_IS = R_IS;
+    pinMode(_RPWM, OUTPUT);
+    pinMode(_LPWM, OUTPUT);
+    pinMode(_L_EN, OUTPUT);
+    pinMode(_R_EN, OUTPUT);
+    pinMode(_L_IS, INPUT);
+    pinMode(_R_IS, INPUT);
+}
+
+void BTS7960::setSpeed(int16_t pwm)
+{
+    if (pwm < -255 || pwm > 255)
+    {
+        // out of range stop the motor
+        stop();
+    }
+    else
+    {
+        if (pwm < 0)
+        {
+            analogWrite(_LPWM, 0);
+            analogWrite(_RPWM, -pwm);
+        }
+        else
+        {
+            analogWrite(_RPWM, 0);
+            analogWrite(_LPWM, pwm);
+        }
+    }
+}
+
+void BTS7960::cwBrake()
+{
+    digitalWrite(_R_EN, 1);
+    digitalWrite(_L_EN, 0);
+}
+
+void BTS7960::ccwBrake()
+{
+    digitalWrite(_R_EN, 0);
+    digitalWrite(_L_EN, 1);
+}
+
+void BTS7960::enable()
+{
+    digitalWrite(_R_EN, 1);
+    digitalWrite(_L_EN, 1);
+}
+
+void BTS7960::disable()
+{
+    digitalWrite(_R_EN, 0);
+    digitalWrite(_L_EN, 0);
+}
+
+void BTS7960::stop()
+{
+    analogWrite(_LPWM, 0);
+    analogWrite(_RPWM, 0);
+}
---
+++ lib/BTS7960/BTS7960.h
@@ -0,0 +1,30 @@
+/*
+  BTS7960.h - Library to control the BTS7960 motor driver
+  Created by Christoph Eickhoff, August 28, 2018.
+  Released into the public domain.
+*/
+#ifndef BTS7960_h
+#define BTS7960_h
+
+#include "Arduino.h"
+
+class BTS7960
+{
+  public:
+    BTS7960(uint8_t RPWM,uint8_t LPWM,uint8_t L_EN,uint8_t R_EN, uint8_t L_IS, uint8_t R_IS);
+    void setSpeed( int16_t pwm);
+    void cwBrake();
+    void ccwBrake();
+    void enable();
+    void disable();
+    void stop();
+
+  private:
+    uint8_t _RPWM;
+    uint8_t _LPWM;
+    uint8_t _L_EN;
+    uint8_t _R_EN;
+    uint8_t _L_IS;
+    uint8_t _R_IS;
+};
+#endif
