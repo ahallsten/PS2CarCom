c2f67f3febe30bcca47227a0fb4705aafe373825
Aunders Hallsten <aehallsten@gmail.com>
2025-10-24 20:55:30 -0600

    axis map doxygen

--- lib/AxisMap/AxisMap.h
+++ lib/AxisMap/AxisMap.h
@@ -3,15 +3,25 @@
 
 #include <Arduino.h>  // if you need map(), constrain(), etc.
 
+/**
+ * @struct
+ * @brief Represents the values that define the dead zone.
+ *
+ * This structure stores the values needed to cleanly map
+ * the positive and negative range of an analog stick axis
+ * to values that control a single range output pwm
+ *
+ */
+
 struct AxisMap {
-  uint8_t low_dead;
-  uint8_t low_from;
-  uint8_t low_to;
-  uint8_t high_dead;
-  uint8_t high_from;
-  uint8_t high_to;
-  uint8_t out_min;
-  uint8_t out_max;
+  uint8_t low_dead;  /**< @brief The y-coordinate of the point. */
+  uint8_t low_from;  /**< @brief The y-coordinate of the point. */
+  uint8_t low_to;  /**< @brief The y-coordinate of the point. */
+  uint8_t high_dead; /**< @brief The y-coordinate of the point. */
+  uint8_t high_from; /**< @brief The y-coordinate of the point. */
+  uint8_t high_to; /**< @brief The y-coordinate of the point. */
+  uint8_t out_min; /**< @brief The y-coordinate of the point. */
+  uint8_t out_max; /**< @brief The y-coordinate of the point. */
 };
 
 uint8_t mapAxis(uint8_t raw, const AxisMap &m);
